c37837e88c0e542f9964356b571bac764359f0a5
Edaena Salinas Jasso <edaenasj@microsoft.com>
2018-11-29 15:57:33 -0800

    Change application

--- application.py
+++ application.py
@@ -7,7 +7,10 @@ app = Sanic()
 async def test(request):
     return json({"hello": "world"})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8000)
 
-app.run()
+#app.run()
+app.run(host= '0.0.0.0', port=80)
+#print ('exiting...')
+#sys.exit(0)
